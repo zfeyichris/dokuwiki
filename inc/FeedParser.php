1018bc24ba762b7c4e55d2fa70e1e297796e6faa
Chris <zfeyichris@gmail.com>
2023-09-01 14:02:52 +0800

    first commit

---
+++ inc/FeedParser.php
@@ -0,0 +1,31 @@
+<?php
+
+use dokuwiki\FeedParserFile;
+use SimplePie\File;
+
+/**
+ * We override some methods of the original SimplePie class here
+ */
+class FeedParser extends \SimplePie\SimplePie {
+
+    /**
+     * Constructor. Set some defaults
+     */
+    public function __construct(){
+        parent::__construct();
+        $this->enable_cache(false);
+        $this->registry->register(File::class, FeedParserFile::class);
+    }
+
+    /**
+     * Backward compatibility for older plugins
+     *
+     * phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     * @param string $url
+     */
+    public function feed_url($url){
+        $this->set_feed_url($url);
+    }
+}
+
+
